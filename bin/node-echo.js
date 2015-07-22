fb464dfda349ff6177c7d6e42699c9156e249d5b
杨帆 <jackyangfan@corp.netease.com>
2015-07-22 16:28:39 +0800

    MISC: echo 命令行

--- bin/node-echo.js
+++ bin/node-echo.js
@@ -1,4 +1,6 @@
 var argv = require('argv'),
     echo = require('../lib/echo');
+//console.log(process.argv[2]);
+console.log(echo(process.argv.join(' ')));
 //console.log(echo(argv.join(' ')));
-console.log(echo('9888'));
+
